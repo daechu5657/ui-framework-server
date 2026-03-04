f42a8ed03b0022155edc751576892e04db6c43a2
daeho KIm <kng02068@naver.com>
2026-03-04 21:28:50 +0900

    feat: add public schema controller

---
+++ UiFrameworkServer/Controllers/PublicController.cs
@@ -0,0 +1,97 @@
+using Microsoft.AspNetCore.Mvc;
+using UiFrameworkServer.Contract.Enums.ComponentManifests;
+using UiFrameworkServer.Contract.Enums.Designs;
+using UiFrameworkServer.Contract.Models.ComponentManifests;
+
+namespace UiFrameworkServer.Controllers
+{
+    [ApiController]
+    public class PublicController : BaseController
+    {
+        public PublicController(IServiceProvider serviceProvider)
+            : base(serviceProvider) { }
+
+        [HttpGet("Schema/ComponentManifestDefinition")]
+        public ActionResult<ComponentManifestDefinition> ComponentManifestDefinitionSchema()
+        {
+            return Ok(
+                new ComponentManifestDefinition
+                {
+                    Name = "Button",
+                    TagName = "Button",
+                    BaseProps =
+                    [
+                        new ComponentManifestPropsStylePropertyDefinition
+                        {
+                            Value =
+                            [
+                                new ComponentManifestPropsStyleDefinition
+                                {
+                                    Key = "backgroundColor",
+                                    Name = "Background Color",
+                                    CssProperty = "background-color",
+                                    ValueType = StyleValueType.String,
+                                    Unit = null,
+                                },
+                            ],
+                        },
+                        new ComponentManifestPropsBehaviorPropertyDefinition
+                        {
+                            Value =
+                            [
+                                new ComponentManifestPropsBehaviorDefinition { Key = "disabled" },
+                            ],
+                        },
+                    ],
+                    Variants = ["default", "ghost"],
+                    VariantOverrides = new(),
+                }
+            );
+        }
+
+        [HttpGet("Schema/ComponentManifestPropsKind")]
+        public ActionResult<ComponentManifestPropsKind> PropsKind()
+        {
+            return Ok(ComponentManifestPropsKind.Style);
+        }
+
+        [HttpGet("Schema/ComponentManifestPropsStyleDefinition")]
+        public ActionResult<ComponentManifestPropsStyleDefinition> StyleDefinition()
+        {
+            return Ok(
+                new ComponentManifestPropsStyleDefinition
+                {
+                    Key = "backgroundColor",
+                    Name = "Background Color",
+                    CssProperty = "background-color",
+                    ValueType = StyleValueType.String,
+                    Unit = null,
+                }
+            );
+        }
+
+        [HttpGet("Schema/ComponentManifestPropsBehaviorDefinition")]
+        public ActionResult<ComponentManifestPropsBehaviorDefinition> BehaviorDefinition()
+        {
+            return Ok(new ComponentManifestPropsBehaviorDefinition { Key = "disabled" });
+        }
+
+        [HttpGet("Schema/ComponentManifestPropsStyleValueKind")]
+        public ActionResult<ComponentManifestPropsStyleValueKind> StyleValueKind()
+        {
+            return Ok(ComponentManifestPropsStyleValueKind.Unset);
+        }
+
+        [HttpGet("Schema/StyleValueType")]
+        public ActionResult<StyleValueType> StyleValueTypeSchema()
+        {
+            return Ok(StyleValueType.String);
+        }
+
+        [HttpGet("Schema/StyleValueUnit")]
+        public ActionResult<StyleValueUnit> StyleValueUnitSchema()
+        {
+            return Ok(StyleValueUnit.Px);
+        }
+    }
+}
